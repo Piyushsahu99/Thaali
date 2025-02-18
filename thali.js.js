d7b83f78c4266ad2936fe28f6a00b405a1e07d11
Piyush sahu <156942206+Piyushsahu99@users.noreply.github.com>
2025-02-18 20:33:39 +0530

    Add files via upload

---
+++ thali.js.js
@@ -0,0 +1,7 @@
+ servicesButton = document.querySelector('.services');
+const servicesContent = document.querySelector('.services-content');
+
+
+servicesButton.addEventListener('click', () => {
+  servicesContent.style.display = servicesContent.style.display === 'services.html' ? 'block' : 'services';
+});const
